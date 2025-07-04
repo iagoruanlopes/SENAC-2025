3206393d99f947681bba55bb89575ec8b52eddfb
iagoruanlopes <ruaniago25@gmail.com>
2025-07-03 22:00:30 -0300

    jogo inicio

--- front-end/pages/javasscript/dom.js
+++ front-end/pages/javasscript/dom.js
@@ -8,6 +8,11 @@ elemento.id = 'btn' // adiciona um id ao elemento
 elemento.className = 'btn' // definir uma classe ao elemento,
 // .classList também pode ser usado se for adicionar mais de uma classe
 
+function Acao(){
+    alert("Ação")
+}
+alert("Depois da Ação")
+
 conainer.appendChild(elemento) // adiciona um elemento filho 
 
 texto = 'Literal'
@@ -17,14 +22,48 @@ conainer.innerHTML += `<button class="${classe}">${texto}</button>` // Literal
 
 var conainer = document.getElementById('container')
 
-conainer.innerHTML = `  
+    var pessoa = [
+    {
+     // JSON -> estrutura de dados para representar um objeto
+         "nome": "lena",
+         "foto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRutXqwF50fonocPg1yFT76GtpBE2uw6z1T4g&s.",
+         "post": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRutXqwF50fonocPg1yFT76GtpBE2uw6z1T4g&s.",
+         "descricao": "text"
+    },{
+  
+         "nome": "Cristiano",
+         "foto": "http://127.0.0.1:5500/front-end/images/77777.jpeg",
+         "post":  "http://127.0.0.1:5500/front-end/images/77777.jpeg",
+         "descricao": "Cristiano"
+    },{
+         "nome": "Cristiano",
+         "foto": "http://127.0.0.1:5500/front-end/images/77777.jpeg",
+         "post": "http://127.0.0.1:5500/front-end/images/77777.jpeg",
+         "descricao": "Cristiano"
+     },{
+         "nome": "Ronaldinho",
+         "foto": "http://127.0.0.1:5500/front-end/images/022.jpg",
+         "post": "http://127.0.0.1:5500/front-end/images/022.jpg",
+         "descricao": "Ronaldinho"
+     },]
+
+for(let i = 0; i<pessoa.length;i++){
+    conainer.innerHTML += `  
         <div class="card">
-            <div class="img"></div>
-            <div class="avatar"></div>
-            <p> texto
-                texto texto
-                texto texto</p>
+
+            <img class="img" src="${pessoa[i].post}" 
+            width="300" height="200"></img>
+
+            <img class="avatar" src="${pessoa[i].foto}"
+            width="50" height="50"></img>
+
+            <p> ${pessoa[i].descricao}</p>
         </div>
 
     </div>
 `
+
+}
+
+
+
---
+++ front-end/pages/javasscript/jogo.js
@@ -0,0 +1,12 @@
+
+let square = document.createElement('div')
+square.classList = 'square'
+ 
+document.body.appendChild(square)
+
+square.addEventListener('click', () => {
+   let a = square 
+   a.style.backgroundColor = "black"
+   document.body.appendChild(a)
+
+})
