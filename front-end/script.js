6edcd94ee1ac12703f469e77bdc0c3660597257f
iagoruanlopes <ruaniago25@gmail.com>
2025-07-16 18:32:31 -0300

    beecrowd

--- front-end/script.js
+++ front-end/script.js
@@ -1,32 +1,64 @@
-// alert("Hello World!")
+//  alert("Hello World!")
 
-// alert(15+33)// soma simples 
+//  alert(15+33)                   // soma simples 
 
-// var a = 55
-// var b = 113
-// alert(a+b) //operações básicas ( +, -, *, /)
+  //  var a = 55
+ //  var b = 113
+//  alert(a+b)                  //operações básicas ( +, -, *, /)
 
-// var n = prompt("Forneça um numero") // receber valor digitado peelo usuários 
-// var numero = parseInt(n) // converte strimg para inteiro ( vide também parse)
-// alert(numero + 10)
+  //  var n = prompt("Forneça um numero") // receber valor digitado peelo usuários 
+ //  var numero = parseInt(n) // converte strimg para inteiro ( vide também parse)
+//  alert(numero + 10)
 
- // var let e const 
-//  var a = 1 // Variável de escopo global 
-//  let b = 2 // Escopo local, não pode ser acessada fora dp escopo
-// const c = 3 // Imultável, pode ser lida, mas não pode  ser reescrita 
-// {  
-   // alert(b); // ReferenceError
-   // let a = 5;
-  //  alert(b); // 5 
-// }
-// alert(b) // 2 
+      //  var let e const 
+     //  var a = 1            // Variável de escopo global 
+    //  let b = 2           // Escopo local, não pode ser acessada fora dp escopo                                                                                                                                                                                                           
+   // 
+  //  const c = 3        // Imultável, pode ser lida, mas não pode  ser reescrita 
+ //  {  
+    //  alert(b);        // ReferenceError
+   //  let a = 5;
+  //  alert(b);      // 5 
+ //  }
+//  alert(b)      // 2 
 
-// Boolean
+//  Boolean
 
-// var boo = false 
-// alert(5===3) // O operador de igualdade ===; outros operadores: <,>,<=,>=.
+ //  var boo = false 
+//  alert(5===3) // O operador de igualdade ===; outros operadores: <,>,<=,>=.
 
+  //  var a = 10
+ //  let b = 20
+//  const c = 30
 
+//  alert(a + " " + b + " " + c)
 
+     //  {
+    //  var  a = 1
+   //  let b = 2
+  //  var d = 5
+ //  alert(a + " " + b + " " + c)
+//  }
 
+//  alert(d)
+ 
+//  var bul = 36 <= 35 && 20 < 30
 
+    //  if (a===b){
+   //  alert('verdadeiro')
+  //  } else {
+ //  alert("falso")
+//  }
+
+  var vetor =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ //            0|1|2|3|4|5|6|7|8|9|
+//  alert(vetor[vetor.length-1])
+ 
+vetor.push(11)        // adiciona item no fim do array
+vetor.pop()          // remover ultimo valor do array
+vetor.unshift(0)    // adiciona no início do array
+vetor.shift()      // remover o primeiro item do array
+
+for(let i = 0;i< vetor.length;i++){
+  alert(vetor[i])
+}
